37df1b3dde74859fb52586d626a2838bcd35fbe1
david rojas <josedavidrojas323@gmail.com>
2025-07-14 04:06:21 -0500

    Auto commit

--- lib/src/core/constants/constants.dart
+++ lib/src/core/constants/constants.dart
@@ -1,3 +1,7 @@
+import 'package:flutter/widgets.dart';
+
 class Constants {
   static const double paddingValue = 6.0;
+  static const EdgeInsets contentPadding = EdgeInsets.all(paddingValue * 2.0);
+  static const double borderRadiusValue = 22.0;
 }
--- lib/src/features/welcome/presentation/page/welcome.dart
+++ lib/src/features/welcome/presentation/page/welcome.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jdphotomap/src/core/constants/constants.dart';
 
 class Welcome extends StatefulWidget {
   const Welcome({super.key});
@@ -16,30 +17,39 @@ class _WelcomeState extends State<Welcome> {
       body: SafeArea(
         child: SizedBox(
           width: double.infinity,
-          child: Column(
-            spacing: 12,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(top: 40),
-                child: SizedBox.square(
-                  dimension: 200,
-                  child: Image.asset('assets/images/logo1.png'),
+          child: Padding(
+            padding: Constants.contentPadding,
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              spacing: Constants.paddingValue,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(top: 40),
+                  child: SizedBox.square(
+                    dimension: 200,
+                    child: Image.asset('assets/images/logo1.png'),
+                  ),
                 ),
-              ),
-              const Text(
-                'Bienvenido a JD PhotoMap',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.w900),
-              ),
-              Text(
-                label,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w400,
+                const Text(
+                  'Bienvenido a JD PhotoMap',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.w900),
                 ),
-              ),
-            ],
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: Constants.paddingValue * 2,
+                  ),
+                  child: Text(
+                    label,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
