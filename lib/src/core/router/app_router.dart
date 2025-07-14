0b1d581c34c137b2100496cb0f20c39721241dda
david rojas <josedavidrojas323@gmail.com>
2025-07-14 01:09:11 -0500

    Auto commit

--- lib/src/core/router/app_router.dart
+++ lib/src/core/router/app_router.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:jdphotomap/src/features/login/presentation/page/login.dart';
+import 'package:jdphotomap/src/features/welcome/presentation/page/welcome.dart';
 
 class AppRouter {
   final GlobalKey<NavigatorState> _rootNavigatorKey =
       GlobalKey<NavigatorState>();
   late final GoRouter router = GoRouter(
     navigatorKey: _rootNavigatorKey,
-    initialLocation: '/',
+    initialLocation: '/welcome',
 
     routes: <RouteBase>[
       GoRoute(
@@ -21,7 +22,7 @@ class AppRouter {
         builder: (_, __) {
           return const Welcome();
         },
-      )
+      ),
     ],
   );
 }
