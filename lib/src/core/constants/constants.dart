adeef67dc8cd897f55e90c5917addaa81f8aabf0
david rojas <josedavidrojas323@gmail.com>
2025-07-14 04:26:30 -0500

    Auto commit

--- lib/src/core/constants/constants.dart
+++ lib/src/core/constants/constants.dart
@@ -6,4 +6,7 @@ class Constants {
   static const double borderRadiusValue = 22.0;
   static const Color primaryColor = Color(0xFF4867B7);
   static const Color secondaryColor = Color(0xFFFFFFFF);
+  static const BorderRadius borderRadius = BorderRadius.all(
+    Radius.circular(borderRadiusValue),
+  );
 }
