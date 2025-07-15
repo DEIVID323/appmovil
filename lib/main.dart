f62a97f3308d2322d776c4c18371b7cde5705e7a
david rojas <josedavidrojas323@gmail.com>
2025-07-15 03:01:21 -0500

    jh

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:jdphotomap/firebase_options.dart';
 import 'package:jdphotomap/src/features/app/presentation/page/app.dart';
 import 'package:jdphotomap/src/injection/injection.dart' as di;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await di.init();
   runApp(const App());
 }
